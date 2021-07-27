a5fb44162119f8b7186bd7456bcbcbc745e63955
cienna <cchung93@my.bcit.ca>
2021-07-27 14:44:16 -0700

    get rid of premade arrays; start organizing to create interactive contact creation

--- src/app.js
+++ src/app.js
@@ -1,36 +1,30 @@
 const contactList = [] //store your contacts as objects
 
-const contact1 = {
-    name: "Bingus Bongus",
-    phoneNum: "604-604-6044",
-    address: "Swamp"
+function getInfo() {
+    let contact = {}
+    
+    //event, card, etc?????
+    return contact
 }
 
-const contact2 = {
-    name: "Bongus Bongus",
-    phoneNum: "604-604-0644",
-    address: "River"
-}
-
-const contact3 = {
-    name: "Bungus Bongus",
-    phoneNum: "604-604-6444",
-    address: "Lake"
-}
-
-function addContacts(){
-    contactList.push(contact1, contact2, contact3)
+function addArray(contact){
+    contactList.push(contact)
     return contactList
 }
 
-function createNav(){
-
+function newContact(){
+    const addContact = document.querySelector("#contact-list")
+    addContact.addEventListener("click", addArray(getInfo()))
+    return contactList
 }
+
+//click new contact (event, storage). open up contact template (editable, separate, storage (literals?)). prompt for name, phone, email. store as array, push to contactList (AAAAAAAAAA).
 
 //Clicking on the 'New Contact' button should open the view to create a contact. 
 //For now you just need the following info: name, phone, email
 //You can use input fields, or content editable divs for creating a contact.
 //When the save button is clicked the placeholder image and contacts name are added to the sidebar list. The user is also returned to the default view with the 'New Contact' button.
+
 //When a name on the sidebar is clicked the contact info is displayed along with the 'Delete Contact' and 'Edit Contact' buttons.
 //When the delete contact button is clicked the contact should be removed from the array as well as the sidebar menu.
 //The 'Edit Contact' button doesn't need to work for the exam.
